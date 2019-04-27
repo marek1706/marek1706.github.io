e24b22f951bfc6f5256ba8deeba1206aab0834af
Marek Zawada <marekz@poczta.onet.pl>
2019-04-27 14:26:40 +0200

    'object-dodatek'

--- JAVASCRIPT/1-javascript/js/7-objects.js
+++ JAVASCRIPT/1-javascript/js/7-objects.js
@@ -8,16 +8,15 @@ let kaja = {
     }
 }
 
-let krystian = {
-    imie: "Krystian",
-    wzrost: 180,
-    przedstawOsobe() {
-        console.log(this.imie);
-    }
-}
+// let krystian = {
+    // imie: "Krystian",
+    // wzrost: 180,
+    // przedstawOsobe() {
+        // console.log(this.imie);}}
+    
 
 kaja.przedstawOsobe();
-krystian.przedstawOsobe();
+// krystian.przedstawOsobe();
 
 console.log(kaja.wzrost);
 
@@ -33,3 +32,24 @@ let monetaZl = {
 }
 
 console.log(monetaZl.material);
+
+// nazwy klas z dużej litery
+
+class Osoba {
+    constructor(imie, nazwisko, wzrost, oczy) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wzrost = wzrost;
+        this.oczy = oczy;
+    }
+
+
+    wyswietlnfo() {
+        console.log("Imię" + this.imie +"\n" + "Nazwisko: " + this.nazwisko + "\n" + "Wzrost: "+ this.wzrost + "\n" + "Oczy: " + this.oczy);
+    }
+}
+
+let krystian = new Osoba('Krystian', 'Dziopa', 180, 'niebieskie');
+let andrzej = new Osoba('Andrzej', 'Szymański', 186, 'zielone');
+
+console.log(krystian,andrzej);
