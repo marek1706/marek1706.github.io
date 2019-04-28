ff3b4321d71b8e068b2eae218403fd26cf8501e6
Marek Zawada <marekz@poczta.onet.pl>
2019-04-28 09:30:08 +0200

    'model-dom'

---
+++ MODELDOM/js/1-model-dom.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log(window);
+console.log(window.interHeight);
+console.log(window.outerHeight);
+
+
+
+console.log(document);
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
